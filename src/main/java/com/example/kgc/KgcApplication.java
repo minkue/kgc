b06fbc872bf5781d905ae05a21935cbc10248569
minkue <minkue0321@naver.com>
2023-05-11 16:40:55 +0900

    first commit

---
+++ src/main/java/com/example/kgc/KgcApplication.java
@@ -0,0 +1,13 @@
+package com.example.kgc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KgcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KgcApplication.class, args);
+	}
+
+}
